84d9b43ae6a0d41c279437c7dcfe5c1c83a91361
Zach <z61@zeesixtyone.com>
2015-02-11 23:26:27 -0500

    Initial implementation of the server class in an actual project

--- xChatLib/Server/Objects.cs
+++ xChatLib/Server/Objects.cs
@@ -4,7 +4,9 @@ using System.Text;
 using System.Drawing;
 using System.Drawing.Imaging;
 using xChatLib.Networking;
-namespace xChatLib
+using xChatLib.Core.Events.Server;
+using Newtonsoft.Json;
+namespace xChatLib.Server
 {
     public class xUser
     {
@@ -34,10 +36,48 @@ namespace xChatLib
         }
         #endregion
         #region Events / Delegates
+        /// <summary>
+        /// Fired when the user connects to a server.
+        /// Uses the xChatLib.Core.Events.Server.UserConnected class.
+        /// </summary>
+        public event EventHandler<UserConnected> onConnect;
+        /// <summary>
+        /// Fired when the user disconnects from a server.
+        /// </summary>
+        public event EventHandler onDisconnect;
+        /// <summary>
+        /// Fired when a Heartbeat packet is received.
+        /// </summary>
+        public event EventHandler onHeartbeat;
         #endregion
         #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public xUser() {
+            Image = null;
+            Name = null;
+
+        }
         #endregion 
         #region Methods
         #endregion
+    }
+    public class Config
+    {
+        [JsonProperty("port")]
+        public ushort port { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+        [JsonProperty("welcomeMessage")]
+        public string welcomeMessage { get; set; }
+        [JsonProperty("whiteList")]
+        public string whiteList { get; set; }
+        [JsonProperty("blackList")]
+        public string blackList {get; set;}
+        [JsonProperty("key")]
+        public string key { get; set; }
+        [JsonProperty("admins")]
+        public UInt32[] AdminIds { get; set; }
     }
 }
--- xChatLib/Server/xChatServer.cs
+++ xChatLib/Server/xChatServer.cs
@@ -4,19 +4,60 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using xChatLib.Networking;
-namespace xChatLib
+namespace xChatLib.Server
 {
     public class xChatServer
     {
         #region Objects
         private AsyncTcpServer _Server = null;
-        
+        private Config _config = null;
         #endregion
         #region Accessors
+        public AsyncTcpServer Server {
+            get { return _Server; }
+            set { _Server = value; }
+        }
+        public Config Configuration
+        {
+            get { return _config; }
+            set { _config = value; }
+        }
         #endregion
         #region Constructors
+        public xChatServer()
+        {
+            
+        }
+        public xChatServer(Config configuration)
+        {
+            try
+            {
+                Configuration = configuration;
+                setupConsole();
+                Console.WriteLine("Creating xChat Server Instance via Configuration given.");
+                Console.WriteLine("Name:" + Configuration.name);
+                Console.WriteLine("Port:" + Configuration.port);
+                run();
+            }
+            catch (Exception ex)
+            {
+                ConsoleColor c = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Error: " + ex.Message);
+                Console.ForegroundColor = c;
+
+            }
+        }
         #endregion
         #region Methods
+        void setupConsole()
+        {
+            Console.Title = "xChat Server";
+        }
+        void run()
+        {
+            Server = new AsyncTcpServer(Configuration.port, Configuration.key);
+        }
         #endregion
     }
 }

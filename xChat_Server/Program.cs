a8cd86ee70c11a3246b0e691841441e87a476323
Zach <z61@zeesixtyone.com>
2015-02-11 23:28:14 -0500

    Added project

---
+++ xChat_Server/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using xChatLib.Server;
+
+namespace xChat_Server
+{
+    class Program
+    {
+        static string filePath = string.Empty;
+        static void Main(string[] args)
+        {
+            if (!System.IO.File.Exists(AppDomain.CurrentDomain.BaseDirectory + "config.json"))
+            {
+                filePath = AppDomain.CurrentDomain.BaseDirectory + "config-default.json";
+            }
+            Configuration config = new Configuration();
+            xChatServer s = new xChatLib.Server.xChatServer(config.readConfig(filePath));
+           
+            Console.Read();
+        }
+    }
+}
